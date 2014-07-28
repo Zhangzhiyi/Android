21891c21e975ddebd640197cdf4037241db41062
Zhangzhiyi <qq361119908@gmail.com>
2014-07-28 18:36:11 +0800

    测试触摸

--- TestTouchEvent/src/com/et/TestTouchEvent/MainActivity.java
+++ TestTouchEvent/src/com/et/TestTouchEvent/MainActivity.java
@@ -12,62 +12,95 @@ import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 
 public class MainActivity extends Activity {
-	public static final String TAG = "MainActivity$MyTextView";
+	public static final String TAG_LinearLayout = "MainActivity$LinearLayout";
+	public static final String TAG_TextView = "MainActivity$MyTextView";
 	private LinearLayout1 linearLayout1;
 	private MyTextView myTextView;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        linearLayout1= (LinearLayout1) findViewById(R.id.LinearLayout);
-        myTextView = (MyTextView) findViewById(R.id.tv);
-        myTextView.setOnTouchListener(new OnTouchListener() {
-			
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		linearLayout1 = (LinearLayout1) findViewById(R.id.LinearLayout);
+		myTextView = (MyTextView) findViewById(R.id.tv);
+		myTextView.setOnTouchListener(new OnTouchListener() {
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				// TODO Auto-generated method stub
 				int action = event.getAction();
 				int x = (int) event.getX();
 				int y = (int) event.getY();
-				//Log.i(TAG + " onTouch", "x = "+ x + " y =" + y);
+				// Log.i(TAG + " onTouch", "x = "+ x + " y =" + y);
 				switch (action) {
-					case MotionEvent.ACTION_DOWN:
-						Log.i(TAG + " onTouch", "MotionEvent.ACTION_DOWN");
-						/**
-						 * 这里返回true就消费了此事件，就不会运行在MyTextView重写onTouchEvent方法down事件，
-						 * 而返回false就会再执行一次MyTextView类重写onTouchEvent方法；
-						 * 注意：如果这里返回false,onTouchEvent方法也是返回false,那么这里和onTouchEvent也会接收
-						 * 不到后续move、up事件;而此时这里返回true，onTouchEvent方法返回false，这里也
-						 * 可以接收到后续的move、up事件，因为这里返回true，所以不会再次运行onTouchEvent方法down事件;**/
-//						return false;
-						//linearLayout1.requestDisallowInterceptTouchEvent(true);//还是运行了跳过了linearLayout1的onInterceptTouchEvent的down事件，但是没有收到后续的move事件和up事件
-						break;
-					case MotionEvent.ACTION_MOVE:
-						Log.i(TAG + " onTouch", "MotionEvent.ACTION_MOVE");
-						/**
-						 * 这里返回true就消费了此事件，就不会运行在MyTextView重写onTouchEvent方法move事件，
-						 * 而返回false就会再执行一次MyTextView类重写onTouchEvent方法move事件，
-						 * 传送move事件onTouchEvent返回值不影响后续接收事件
-						 * **/
-						//return true;
-						//linearLayout1.requestDisallowInterceptTouchEvent(true); //跳过了linearLayout1的onInterceptTouchEvent的move事件和up事件
-						break;
-					case MotionEvent.ACTION_UP:
-						Log.i(TAG + " onTouch", "MotionEvent.ACTION_UP");
-						/**
-						 * 这里返回true就消费了此事件，就不会运行在MyTextView重写onTouchEvent方法up事件，
-						 * 而返回false就会再执行一次MyTextView类重写onTouchEvent方法up事件，
-						 * 传送up事件onTouchEvent返回值不影响后续接收事件
-						 * **/
-//						return true;
-						break;
-					case MotionEvent.ACTION_CANCEL:
-						Log.i(TAG + " onTouch", "MotionEvent.ACTION_CANCEL");
-						break;
+				case MotionEvent.ACTION_DOWN:
+					Log.i(TAG_TextView + " onTouch", "MotionEvent.ACTION_DOWN");
+					/**
+					 * 这里返回true就消费了此事件，就不会运行在MyTextView重写onTouchEvent方法down事件，
+					 * 而返回false就会再执行一次MyTextView类重写onTouchEvent方法；
+					 * 注意：如果这里返回false
+					 * ,onTouchEvent方法也是返回false,那么这里和onTouchEvent也会接收
+					 * 不到后续move、up事件;而此时这里返回true，onTouchEvent方法返回false，这里也
+					 * 可以接收到后续的move
+					 * 、up事件，因为这里返回true，所以不会再次运行onTouchEvent方法down事件;
+					 **/
+					// return false;
+					// linearLayout1.requestDisallowInterceptTouchEvent(true);//还是运行了跳过了linearLayout1的onInterceptTouchEvent的down事件，但是没有收到后续的move事件和up事件
+					break;
+				case MotionEvent.ACTION_MOVE:
+					Log.i(TAG_TextView + " onTouch", "MotionEvent.ACTION_MOVE");
+					/**
+					 * 这里返回true就消费了此事件，就不会运行在MyTextView重写onTouchEvent方法move事件，
+					 * 而返回false就会再执行一次MyTextView类重写onTouchEvent方法move事件，
+					 * 传送move事件onTouchEvent返回值不影响后续接收事件
+					 * **/
+					// return true;
+					// linearLayout1.requestDisallowInterceptTouchEvent(true);
+					// //跳过了linearLayout1的onInterceptTouchEvent的move事件和up事件
+					break;
+				case MotionEvent.ACTION_UP:
+					Log.i(TAG_TextView + " onTouch", "MotionEvent.ACTION_UP");
+					/**
+					 * 这里返回true就消费了此事件，就不会运行在MyTextView重写onTouchEvent方法up事件，
+					 * 而返回false就会再执行一次MyTextView类重写onTouchEvent方法up事件，
+					 * 传送up事件onTouchEvent返回值不影响后续接收事件
+					 * **/
+					// return true;
+					break;
+				case MotionEvent.ACTION_CANCEL:
+					Log.i(TAG_TextView + " onTouch", "MotionEvent.ACTION_CANCEL");
+					break;
 				}
 				return false;
 			}
 		});
-    }
+		
+		/***
+		 * 如果ViewGroup里面的子控件能够处理触摸事件，是不会运行ViewGroup的OnTouchListener的
+		 */
+		linearLayout1.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				int action = event.getAction();
+				switch (action) {
+				case MotionEvent.ACTION_DOWN:
+					Log.i(TAG_LinearLayout + " onTouch", "MotionEvent.ACTION_DOWN");
+					break;
+				case MotionEvent.ACTION_MOVE:
+					Log.i(TAG_LinearLayout + " onTouch", "MotionEvent.ACTION_MOVE");
+					break;
+				case MotionEvent.ACTION_UP:
+					Log.i(TAG_LinearLayout + " onTouch", "MotionEvent.ACTION_UP");
+					break;
+				case MotionEvent.ACTION_CANCEL:
+					Log.i(TAG_LinearLayout + " onTouch", "MotionEvent.ACTION_CANCEL");
+					break;
+				}
+				return false;
+			}
+		});
+	}
 }
--- TestViewPager/src/com/et/TestViewPager/TestViewPagerActivity.java
+++ TestViewPager/src/com/et/TestViewPager/TestViewPagerActivity.java
@@ -24,99 +24,110 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class TestViewPagerActivity extends FragmentActivity {
-	
+
 	public static final String TAG = "TestViewPagerActivity";
-	ViewPager mViewPager ;
+	ViewPager mViewPager;
 	FragmentPagerAdater pagerAdater;
 	PagerTabStrip pagerTabStrip;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        mViewPager = (ViewPager) findViewById(R.id.viewpagerLayout);
-        pagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
-        pagerTabStrip.setTabIndicatorColorResource(android.R.color.white);
-        ArrayList<Integer> lists = new ArrayList<Integer>();
-        lists.add(R.drawable.drawable1);
-        lists.add(R.drawable.drawable2);
-        lists.add(R.drawable.drawable3);
 
-        pagerAdater = new FragmentPagerAdater(getSupportFragmentManager());
-        mViewPager.setAdapter(pagerAdater);
-//        mViewPager.setAdapter(new ViewPagerAdater(this, lists));   pagerTabStrip只对PagerAdapter有效
-        mViewPager.setCurrentItem(6000);
-        mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
-        	private final static String TAG = "OnPageChangeListener";
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mViewPager = (ViewPager) findViewById(R.id.viewpagerLayout);
+		pagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
+		pagerTabStrip.setTabIndicatorColorResource(android.R.color.white);
+		ArrayList<Integer> lists = new ArrayList<Integer>();
+		lists.add(R.drawable.drawable1);
+		lists.add(R.drawable.drawable2);
+		lists.add(R.drawable.drawable3);
+
+		pagerAdater = new FragmentPagerAdater(getSupportFragmentManager());
+		mViewPager.setAdapter(pagerAdater);
+		// mViewPager.setAdapter(new ViewPagerAdater(this, lists));
+		// pagerTabStrip只对PagerAdapter有效
+		mViewPager.setCurrentItem(6000);
+		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+			private final static String TAG = "OnPageChangeListener";
+
 			@Override
 			public void onPageSelected(int position) {
 				// TODO Auto-generated method stub
 				Log.i("onPageSelected", "position:" + position);
 			}
-			
+
 			@Override
 			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 				// TODO Auto-generated method stub
-				Log.i("onPageScrolled", "position:" + position + "|" + "positionOffset:" + positionOffset + "|" + "positionOffsetPixels:" + positionOffsetPixels);
+				Log.i("onPageScrolled", "position:" + position + "|" + "positionOffset:" + positionOffset + "|" + "positionOffsetPixels:"
+						+ positionOffsetPixels);
 			}
-			
+
 			@Override
 			public void onPageScrollStateChanged(int state) {
 				// TODO Auto-generated method stub
 				switch (state) {
-					case ViewPager.SCROLL_STATE_IDLE:
-						Log.i("onPageScrollStateChanged", "SCROLL_STATE_IDLE");
-						break;
-					case ViewPager.SCROLL_STATE_DRAGGING:
-						Log.i("onPageScrollStateChanged", "SCROLL_STATE_DRAGGING");
-						break;
-					case ViewPager.SCROLL_STATE_SETTLING:
-						Log.i("onPageScrollStateChanged", "SCROLL_STATE_SETTLING");
-						break;
-					
+				case ViewPager.SCROLL_STATE_IDLE:
+					Log.i("onPageScrollStateChanged", "SCROLL_STATE_IDLE");
+					break;
+				case ViewPager.SCROLL_STATE_DRAGGING:
+					Log.i("onPageScrollStateChanged", "SCROLL_STATE_DRAGGING");
+					break;
+				case ViewPager.SCROLL_STATE_SETTLING:
+					Log.i("onPageScrollStateChanged", "SCROLL_STATE_SETTLING");
+					break;
+
 				}
 			}
 		});
-        //控制循环滚动的handler
-        final Handler handler = new Handler(){
-        	@Override
-        	public void handleMessage(Message msg) {
-        		// TODO Auto-generated method stub
-        		int curItem = mViewPager.getCurrentItem();
-				//mViewPager.setCurrentItem(curItem + 1 , true);
+		// 控制循环滚动的handler
+		final Handler handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				int curItem = mViewPager.getCurrentItem();
+//				mViewPager.setCurrentItem(curItem + 1, true);
 				sendEmptyMessageDelayed(1, 3000);
-        	}
-        };
-        handler.sendEmptyMessageDelayed(1, 5000);
-        mViewPager.setOnTouchListener(new OnTouchListener() {
-			
+			}
+		};
+		handler.sendEmptyMessageDelayed(1, 5000);
+		mViewPager.setOnTouchListener(new OnTouchListener() {
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				// TODO Auto-generated method stub
 				switch (event.getAction()) {
-					case MotionEvent.ACTION_DOWN:
-						handler.removeMessages(1);
-						return false;
-					case MotionEvent.ACTION_UP:
-					case MotionEvent.ACTION_CANCEL:
-						handler.sendEmptyMessageDelayed(1, 5000);
-						return false;
+				case MotionEvent.ACTION_DOWN:
+					Log.i(TAG, "MotionEvent.ACTION_DOWN");
+					handler.removeMessages(1);
+					return false;
+				case MotionEvent.ACTION_UP:
+					Log.i(TAG, "MotionEvent.ACTION_DOWN");
+					handler.removeMessages(1);
+					handler.sendEmptyMessageDelayed(1, 5000);
+					return false;
+				case MotionEvent.ACTION_CANCEL:
+					Log.i(TAG, "MotionEvent.ACTION_CANCEL");
+					handler.removeMessages(1);
+					handler.sendEmptyMessageDelayed(1, 5000);
+					return false;
 				}
 				return false;
 			}
 		});
-        /**用反射更改PagerTabStrip里面的私有变量的值**/
-        pagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
-        //获取PagerTabStrip类的名字,  "android.support.v4.view.PagerTabStrip"
-        String className = PagerTabStrip.class.getName();
-        //获取类对象
+		/** 用反射更改PagerTabStrip里面的私有变量的值 **/
+		pagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
+		// 获取PagerTabStrip类的名字, "android.support.v4.view.PagerTabStrip"
+		String className = PagerTabStrip.class.getName();
+		// 获取类对象
 		Class<?> classObject;
 		try {
-			
+
 			classObject = Class.forName(className);
-			//获取私有属性要用declared
+			// 获取私有属性要用declared
 			Field privateField = classObject.getDeclaredField("mIndicatorHeight");
-			//设置不进行语言访问检查
+			// 设置不进行语言访问检查
 			privateField.setAccessible(true);
 			int mIndicatorHeight = privateField.getInt(pagerTabStrip);
 			privateField.setInt(pagerTabStrip, 0);
@@ -133,18 +144,21 @@ public class TestViewPagerActivity extends FragmentActivity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-    /**可以接受到其它工程Activity的返回值**/
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	// TODO Auto-generated method stub
-    	Log.i(TAG, "onActivityResult");
-    }
-    public class FragmentPagerAdater extends FragmentStatePagerAdapter{
-    	public FragmentPagerAdater(FragmentManager fm) {
+	}
+
+	/** 可以接受到其它工程Activity的返回值 **/
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		Log.i(TAG, "onActivityResult");
+	}
+
+	public class FragmentPagerAdater extends FragmentStatePagerAdapter {
+		public FragmentPagerAdater(FragmentManager fm) {
 			// TODO Auto-generated constructor stub
-    		super(fm);
+			super(fm);
 		}
+
 		@Override
 		public Fragment getItem(int position) {
 			// TODO Auto-generated method stub
@@ -156,31 +170,56 @@ public class TestViewPagerActivity extends FragmentActivity {
 			// TODO Auto-generated method stub
 			return Integer.MAX_VALUE;
 		}
-    	
-    }
-    public static class ArrayListFragment extends Fragment{
-    	static ArrayListFragment newInstance(int num) {
-            ArrayListFragment f = new ArrayListFragment();
-            Bundle args = new Bundle();
-            args.putInt("num", num);
-            f.setArguments(args);
-            return f;
-        }
-    	@Override
-    	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    		// TODO Auto-generated method stub
-    		LinearLayout layout = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.innerpager_item_layout, container, false);
-    		
+
+	}
+
+	public static class ArrayListFragment extends Fragment {
+		static ArrayListFragment newInstance(int num) {
+			ArrayListFragment f = new ArrayListFragment();
+			Bundle args = new Bundle();
+			args.putInt("num", num);
+			f.setArguments(args);
+			return f;
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			LinearLayout layout = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.innerpager_item_layout, container, false);
+			
+			//Banner
 			ViewPager viewPager = (ViewPager) layout.findViewById(R.id.viewPager);
 			ViewPagerDot viewPagerDot = (ViewPagerDot) layout.findViewById(R.id.details_dot);
 			ArrayList<Integer> lists = new ArrayList<Integer>();
-	        lists.add(R.drawable.drawable1);
-	        lists.add(R.drawable.drawable2);
-	        lists.add(R.drawable.drawable3);
+			lists.add(R.drawable.drawable1);
+			lists.add(R.drawable.drawable2);
+			lists.add(R.drawable.drawable3);
 			MyInnerPagerAdapter pagerAdater = new MyInnerPagerAdapter(getActivity(), lists);
 			viewPager.setAdapter(pagerAdater);
 			viewPagerDot.setViewPager(viewPager);
-			
+			viewPager.setOnTouchListener(new OnTouchListener() {
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					switch (event.getAction()) {
+					case MotionEvent.ACTION_DOWN:
+						Log.i(TAG , "Banner:" + "MotionEvent.ACTION_DOWN");
+						return false;
+					case MotionEvent.ACTION_MOVE:
+						Log.i(TAG, "Banner:" + "MotionEvent.ACTION_MOVE");
+						break;
+					case MotionEvent.ACTION_UP:
+						Log.i(TAG, "Banner:" + "MotionEvent.ACTION_UP");
+						return false;
+					case MotionEvent.ACTION_CANCEL:
+						Log.i(TAG, "Banner:" + "MotionEvent.ACTION_CANCEL");
+						return false;
+					}
+					return false;
+				}
+			});
+			//Banner + ListView
 			View headerView = LayoutInflater.from(getActivity()).inflate(R.layout.listview_header, null);
 			ViewPager headViewPager = (ViewPager) headerView.findViewById(R.id.viewPager);
 			ViewPagerDot headPagerDot = (ViewPagerDot) headerView.findViewById(R.id.details_dot);
@@ -191,17 +230,41 @@ public class TestViewPagerActivity extends FragmentActivity {
 			MyInnerPagerAdapter headPagerAdater = new MyInnerPagerAdapter(getActivity(), headlists);
 			headViewPager.setAdapter(headPagerAdater);
 			headPagerDot.setViewPager(headViewPager);
-			
+
 			ListView listView = (ListView) layout.findViewById(R.id.listView1);
 			ListAdapter listdapter = new ListAdapter(getActivity());
 			listView.addHeaderView(headerView);
 			listView.setAdapter(listdapter);
+			
+			headViewPager.setOnTouchListener(new OnTouchListener() {
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					switch (event.getAction()) {
+					case MotionEvent.ACTION_DOWN:
+						Log.i(TAG , "Banner + ListView:" + "MotionEvent.ACTION_DOWN");
+						return false;
+					case MotionEvent.ACTION_MOVE:
+						Log.i(TAG, "Banner + ListView:" + "MotionEvent.ACTION_MOVE");
+						break;
+					case MotionEvent.ACTION_UP:
+						Log.i(TAG, "Banner + ListView:" + "MotionEvent.ACTION_UP");
+						return false;
+					case MotionEvent.ACTION_CANCEL:
+						Log.i(TAG, "Banner + ListView:" + "MotionEvent.ACTION_CANCEL");
+						return false;
+					}
+					return false;
+				}
+			});
 			return layout;
-    	}
-    	@Override
-    	public void onActivityCreated(Bundle savedInstanceState) {
-    		// TODO Auto-generated method stub
-    		super.onActivityCreated(savedInstanceState);
-    	}
-    }
+		}
+
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			// TODO Auto-generated method stub
+			super.onActivityCreated(savedInstanceState);
+		}
+	}
 }
